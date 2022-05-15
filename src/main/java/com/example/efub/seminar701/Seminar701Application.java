6479a696e52f4615ce80f0705e477ae95a95c2cd
seojung <chseojung@naver.com>
2022-05-15 18:59:14 +0900

    Initial commit

---
+++ src/main/java/com/example/efub/seminar701/Seminar701Application.java
@@ -0,0 +1,13 @@
+package com.example.efub.seminar701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar701Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/efub/seminar701/Seminar701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efub.seminar701;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar701ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
